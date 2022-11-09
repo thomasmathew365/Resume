19221054cbcf177aaf476aaee09a3804282f40c6
Thomas <thomas.mathew@futureplay.co>
2022-11-09 15:55:50 +0900

    Added Sanity CMS

---
+++ tom-resume-cms/schemas/home.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'home',
+    type: 'document',
+      title: 'Home',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+      {
+        name: 'age',
+        type: 'string',
+        title: 'Age'
+      }
+    ]
+  }
---
+++ tom-resume-cms/schemas/schema.js
@@ -0,0 +1,14 @@
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type';
+import createSchema from 'part:@sanity/base/schema-creator';
+
+import home from './home';
+
+// First, we must import the schema creator
+// Then we give our schema to the builder and provide the result to Sanity
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    home
+  ]),
+})
